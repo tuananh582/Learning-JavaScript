008f8a2f7c1221f81450f993647f067702709361
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-07-16 16:00:17 +0700

    new

--- datastructerjs/destructuringobject/main.js
+++ datastructerjs/destructuringobject/main.js
@@ -21,8 +21,26 @@ const restaurant ={
 
     order:function(starterIndex,mainIndex){
         return [this.startermenu[starterIndex],this.mainMenu[mainIndex]];
-    }
+    },
+    // orderDeliver:function(obj){  
+    //     console.log(obj)
+    // },
+    orderDeliver:function({starterIndex=1,mainIndex=0,time='20:00',address}){ // We received object and we do immediately destructuring  
+        console.log(`Order received ${this.startermenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} ant at ${time} `)
+    },
  };
+
+ restaurant.orderDeliver({
+    time:'22:30',
+    address:'United-Kingdom',
+    mainIndex:2,
+    starterIndex:2,
+ });
+ restaurant.orderDeliver({
+    address:'London',
+    starterIndex:1,
+ });
+
  const {name,openingHours,categories}=restaurant;
  console.log(name,openingHours,categories);
 
@@ -32,4 +50,25 @@ const restaurant ={
     openingHours:hours,
     categories:tags,
 }=restaurant;
- console.log(restaurantName,hours,tags)
+ console.log(restaurantName,hours,tags);
+ 
+ // Default Values
+ const {menu=[],startermenu:starter=[]}=restaurant;
+console.log(menu,starter);
+
+// Mutating variables
+let a =111;
+let b=999;
+const obj = {a:23,b:7,c:14};
+
+ ({a,b}=obj) // Wrap it into parenthesis 
+ console.log(a,b)
+
+ //nested objects
+ const {fri:{open,close}}= openingHours
+ console.log(open,close)
+
+
+//  let [a,,,b]=restaurant.categories;
+//  [a,b]=[b,a]
+//  console.log(a,b)
