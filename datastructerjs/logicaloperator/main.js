5d18f39b4882be1c4d383bcc07d5569be16655bf
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-07-18 00:19:46 +0700

    push new logical operator

---
+++ datastructerjs/logicaloperator/main.js
@@ -0,0 +1,28 @@
+const rest1={
+    name:'Capri',
+    namGuests:0,
+};
+const rest2={
+    name:'La Piazaa',
+    owener:'Giovanni Rossi',
+};
+
+// // OR assignment operator
+// rest1.namGuests= rest1.namGuests || 10;
+// rest2.namGuests= rest2.namGuests || 10;
+
+// rest1.namGuests||=10
+// rest2.namGuests||=10
+console.log(rest2)
+console.log('after')
+ // // nullish assignment operator
+rest1.namGuests??=10
+rest2.namGuests??=10
+// rest1.owener=  rest1.owener && 'Annymous';// Way 1 if it false will return undefined
+// rest2.owener=  rest2.owener && 'Annymous';
+//Way 2 if false will return 0 :
+rest1.owener&&="Annoymus"
+rest2.owener&&='Annoymus'
+
+console.log(rest1)
+console.log(rest2)
