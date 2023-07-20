3f9ab35882aa666e9288641aeb2d6163b8701295
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-07-20 16:48:54 +0700

    push set

---
+++ datastructerjs/set/main.js
@@ -0,0 +1,76 @@
+'use strict ';
+const weekdays=['mon','tue','wed','thu','fri','sat','sun'];
+// const [,,,thu,fri,sat]=weekdays
+const hours={
+    [weekdays[3]]:{
+        open:12,
+        close:22,
+    },
+    [weekdays[4]]:{
+        open:11,
+        close:23,
+    },
+    [`day-${2+4}`]:{
+        open:0, // open 24 hours
+        close:24,
+    },
+};
+
+
+
+
+const restaurant ={
+    name :'Tuan Anh',
+    location:'New York',
+    categories:['Italian','vegetarian','Pizzeria','Organic'],
+    startermenu:['Garlic','Bread','Focaccia'],
+    mainMenu:['Pizza','Pasta','Rissto'],
+    // enhanced object literals
+    // openingHours:openingHours,
+    hours,
+
+    order(starterIndex,mainIndex){
+        return [this.startermenu[starterIndex],this.mainMenu[mainIndex]];
+    },
+    // orderDeliver:function(obj){  
+    //     console.log(obj)
+    // },
+    orderDeliver({starterIndex=1,mainIndex=0,time='20:00',address}){ // We received object and we do immediately destructuring  
+        console.log(`Order received ${this.startermenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} ant at ${time} `)
+    },
+
+    oderPasta(ing1,ing2,ing3){
+        console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`)
+    },
+
+    oderPizza(MainIngredient,...otherIngredient){
+        console.log(MainIngredient)
+        console.log(otherIngredient)
+    }
+ };
+// set will distinguish seperated array
+ const orderSet= new Set([
+    'Pasta','Pizza','Pizza','Risotto','Pasta','Pizza'
+]);
+console.log(orderSet)
+console.log(new Set('Jonas'))
+console.log(orderSet.size)
+console.log(orderSet.has('Pizza'))
+console.log(orderSet.has('Pasta'))
+console.log(orderSet.has('Nonny'))
+orderSet.add('Garlic Bread')
+orderSet.add('Garlic Bread')
+// orderSet.delete('Risotto')
+// orderSet.clear()
+console.log(orderSet) //it can not retrieve 
+
+for(const order of orderSet){
+    console.log(order)
+}
+
+const staff =['Waiter','Chef','Waiter','Manager','Chef','Waiter']
+const staffUnique = [...new Set(staff)]
+console.log(staffUnique)
+console.log(new Set(['Waiter','Chef','Waiter','Manager','Chef','Waiter']).size)
+
+console.log(new Set('hduwahsadwazcxfayuykyduhwaudwa').size)//output 12 seperated 
