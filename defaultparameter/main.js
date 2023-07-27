4b1d9cfc13671b0d84553f708cecb5a18c6c1398
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-07-27 15:50:09 +0700

    moi

--- defaultparameter/main.js
+++ defaultparameter/main.js
@@ -20,3 +20,33 @@ creatBooking('Lh123',2,800)
 creatBooking('LH123',2)
 creatBooking('LH123',5)
 creatBooking('LH123',undefined,1000)
+
+// New Lecture
+const flight ='LH234'
+const tuananh={
+    name :'Tuan Anh',
+    passport:3215367215321,
+}
+const checkIn=function(flightnum,passenger){
+    flightnum='LH999'
+    passenger.name='Mr.'+passenger.name
+    if(passenger.passport===3215367215321){
+        alert('Check in ') 
+    }else{
+        alert('Wrong passport')
+    }
+}
+checkIn(flight,tuananh)
+console.log(flight)
+// console.log(tuananh)
+ 
+// Is the same as doing ...
+// const flightnum=flight
+// const passenger=tuananh
+
+const newPassport = function(person){
+    person.passport=Math.trunc(Math.random()*100000000000)
+}
+newPassport(tuananh)
+checkIn(flight,tuananh)
+console.log(tuananh)
