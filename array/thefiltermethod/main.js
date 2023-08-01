602e6e13afd94be91fca6f1a7fefabeb1222d20b
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-01 17:36:11 +0700

    filter

---
+++ array/thefiltermethod/main.js
@@ -0,0 +1,12 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposit = movements.filter(function(mov){
+        return mov>0
+})
+console.log(movements)
+console.log(deposit)
+const depositFor=[]
+for(const mov of movements)if(mov>0) depositFor.push(mov) 
+console.log(depositFor)
+
+const withdraw = movements.filter(mov=>mov<0)
+console.log(withdraw)
