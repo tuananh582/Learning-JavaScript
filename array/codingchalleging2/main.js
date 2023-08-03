86e28a0e8e0b5a999ab8b18e22bb80a337e4a3cc
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-03 15:45:19 +0700

    new

--- array/codingchalleging2/main.js
+++ array/codingchalleging2/main.js
@@ -12,3 +12,13 @@ const calcAverageHumanAge= function(ages){
 const av1=calcAverageHumanAge([5,2,4,1,15,8,3])
 const av2 =calcAverageHumanAge([16,6,10,5,6,1,4])
 console.log(av1,av2)
+
+// Rewriteing coding challleng2 to complete coding challenging 3 
+const rewritechainingwork= (movements)=>{
+    const humanage=movements.map(age=>age<=2?2*age:16+age*4).filter(agedog=>agedog>=18).reduce((acc,cur,i,arr)=>acc+cur/arr.length,0)
+   return humanage
+}
+const rw1=calcAverageHumanAge([5,2,4,1,15,8,3])
+const rw2 =calcAverageHumanAge([16,6,10,5,6,1,4])
+console.log(rw1,rw2)
+
