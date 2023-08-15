02a5d3bc43c7959323233d6cd7091054b1d833f5
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-15 18:27:06 +0700

    new

--- mapty/map/main.js
+++ mapty/map/main.js
@@ -10,14 +10,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
+let map,mapEvent;
 navigator.geolocation?.getCurrentPosition(function (position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
     console.log(latitude, longitude)
     
     const coords=[latitude,longitude]
-    const map = L.map('map').setView(coords, 17);
+     map = L.map('map').setView(coords, 17);
     L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
@@ -26,14 +26,39 @@ navigator.geolocation?.getCurrentPosition(function (position) {
     //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
     //     .openPopup();
 
-    map.on('click',function(mapEvent){
-            console.log(mapEvent)
-            const {lat,lng}=mapEvent.latlng;
-            L.marker([lat,lng]).addTo(map)
-            .bindPopup(L.popup({maxWidth:250,minWidth:100,autoClose:false,closeOnClick:false,className:'running-popup'})).setPopupContent('Hey Working Out')
-             .openPopup();
+    //Handling clicks on map
+    map.on('click',function(mapE){
+            mapEvent=mapE;
+            form.classList.remove('hidden')
+            inputDistance.focus()
+
+            // console.log(mapEvent)
+            // const {lat,lng}=mapEvent.latlng;
+            // L.marker([lat,lng]).addTo(map)
+            // .bindPopup(L.popup({maxWidth:250,minWidth:100,autoClose:false,closeOnClick:false,className:'running-popup'})).setPopupContent('Hey Working Out')
+            //  .openPopup();
     })// coming from leaflet libary
 
 }, function () {
     alert('Could not get your position')
 });
+
+form.addEventListener('submit',function(e){
+    //Clear input Fields
+    inputDistance.value= inputDuration.value =inputCadence.value=inputElevation.value='';
+
+
+    //Display marker
+    e.preventDefault();
+        console.log(mapEvent)
+            const {lat,lng}=mapEvent.latlng;
+            L.marker([lat,lng]).addTo(map)
+            .bindPopup(L.popup({maxWidth:250,minWidth:100,autoClose:false,closeOnClick:false,className:'running-popup'})).setPopupContent('Hey Working Out').openPopup();
+});
+
+inputType.addEventListener('change',function(){
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+})
+
+
