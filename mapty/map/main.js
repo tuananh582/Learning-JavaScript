57ec4ef16a107f64a04ea9c57a9c0ad2e3925a0a
tuananh582 <101263575+tuananh582@users.noreply.github.com>
2023-08-15 17:50:53 +0700

    new

--- mapty/map/main.js
+++ mapty/map/main.js
@@ -22,9 +22,18 @@ navigator.geolocation?.getCurrentPosition(function (position) {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker(coords).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    // L.marker(coords).addTo(map)
+    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //     .openPopup();
+
+    map.on('click',function(mapEvent){
+            console.log(mapEvent)
+            const {lat,lng}=mapEvent.latlng;
+            L.marker([lat,lng]).addTo(map)
+            .bindPopup(L.popup({maxWidth:250,minWidth:100,autoClose:false,closeOnClick:false,className:'running-popup'})).setPopupContent('Hey Working Out')
+             .openPopup();
+    })// coming from leaflet libary
+
 }, function () {
     alert('Could not get your position')
 });
